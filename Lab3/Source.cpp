dccb3e31013c438b7640b2932a31234036d8ed6a
Recreatorms <43077503+Recreatorms@users.noreply.github.com>
2021-01-27 00:00:51 +0300

    Added textures

--- Lab3/Source.cpp
+++ Lab3/Source.cpp
@@ -5,14 +5,18 @@
 #include <math.h>
 #include <sstream>
 
-#include "SOIL.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "./third_party/stb_image.h"
 
 #define WIN_HEIGHT 750
 #define WIN_WIDTH 750
 #define SIDE_LENGTH 10
+#define NUMBER_OF_PARTS 3
 #define TEXTURES_COUNT 8
+
+GLfloat speed = 0.5;
 GLfloat ox_rotation = 0;
-GLfloat dx_rotation = 0.5;
+GLfloat dx_rotation = speed;
 GLfloat oy_rotation = 0;
 GLfloat dy_rotation = 0;
 
@@ -34,11 +38,19 @@ GLuint cutOctahedronOpen;
 
 bool textured = false;
 GLuint textures[TEXTURES_COUNT];
+int textsum = sizeof(textures) / sizeof(textures[0]);
+struct text_img {
+	unsigned char * data;
+	int sizeX, sizeY, n;
+};
+char text_name[8][21] {
+	"./textures/text0.jpg", "./textures/text1.jpg", "./textures/text2.jpg", "./textures/text3.jpg", "./textures/text4.jpg", "./textures/text5.jpg", "./textures/text6.jpg", "./textures/text7.jpg"
+};
 
 GLfloat oct_textures_coordinates[] = {
-		0.0, 0.0,  
-		1.0, 0.0,  
-		0.5, 1.0,  
+		0.0, 0.0,
+		1.0, 0.0,
+		0.5, 1.0,
 		0.0, 0.0,
 		1.0, 0.0,
 		0.5, 1.0,
@@ -61,7 +73,31 @@ GLfloat oct_textures_coordinates[] = {
 		1.0, 0.0,
 		0.5, 1.0
 };
+void load_textures()
+{
+	text_img* texture = new text_img[8];
+	//size_t texture_pos = 0;
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glGenTextures(8, textures);
 
+	for (int i(0), texture_pos(0); i < 8; i++) {
+		texture[i].data = stbi_load(text_name[texture_pos], &texture[i].sizeX, &texture[i].sizeY, &texture[i].n, STBI_rgb);
+		if (texture[i].data == nullptr) {
+			std::cout << "it's seems some troubles with textures loading\n";
+			exit(0);
+		}
+
+		texture_pos++;
+
+		glBindTexture(GL_TEXTURE_2D, textures[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexImage2D(GL_TEXTURE_2D, 0, 3, texture[i].sizeX, texture[i].sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, texture[i].data);
+	}
+
+	delete[] texture;
+}
+/*
 void initTextures() {
 	int width[TEXTURES_COUNT],
 		height[TEXTURES_COUNT];
@@ -71,7 +107,7 @@ void initTextures() {
 
 	std::stringstream filename;
 	for (int i = 0; i < TEXTURES_COUNT; i++) {
-		filename << "./textures/text" << i << ".jpg";
+		filename << "../textures/text" << i << ".jpg";
 		images[i] = SOIL_load_image(filename.str().c_str(), &width[i], &height[i],
 			nullptr, SOIL_LOAD_RGB);
 
@@ -90,7 +126,7 @@ void initTextures() {
 		SOIL_free_image_data(image);
 		glBindTexture(GL_TEXTURE_2D, 0); // Object unlinking
 	}
-}
+}*/
 namespace OCT_FACES {
 	const GLubyte front[] = { 0, 1, 2 };
 	const GLubyte right_side[] = { 3, 4, 5 };
@@ -139,35 +175,35 @@ const GLfloat oct_normals[] = {
 void drawOct() {
 	using namespace OCT_FACES;
 	GLfloat oct_vertices[] = {
-		-SIDE_LENGTH - opened, 0.0f + opened, 0.0f + opened, 
-		0.0f - opened, SIDE_LENGTH + opened, 0.0f + opened,  
-		0.0f - opened, 0.0f + opened, SIDE_LENGTH + opened,  
+		-SIDE_LENGTH - opened, 0.0f + opened, 0.0f + opened,
+		0.0f - opened, SIDE_LENGTH + opened, 0.0f + opened,
+		0.0f - opened, 0.0f + opened, SIDE_LENGTH + opened,
 
-		0.0f + opened, 0.0f + opened, SIDE_LENGTH + opened, 
-		0.0f + opened, SIDE_LENGTH + opened, 0.0f + opened,  
-		SIDE_LENGTH + opened, 0.0f + opened, 0.0f + opened,  
+		0.0f + opened, 0.0f + opened, SIDE_LENGTH + opened,
+		0.0f + opened, SIDE_LENGTH + opened, 0.0f + opened,
+		SIDE_LENGTH + opened, 0.0f + opened, 0.0f + opened,
 
-		SIDE_LENGTH + opened, 0.0f + opened, 0.0f - opened, 
-		0.0f + opened, SIDE_LENGTH + opened, 0.0f - opened,  
+		SIDE_LENGTH + opened, 0.0f + opened, 0.0f - opened,
+		0.0f + opened, SIDE_LENGTH + opened, 0.0f - opened,
 		0.0f + opened, 0.0f + opened, -SIDE_LENGTH - opened,
 
-		0.0f - opened, 0.0f + opened, -SIDE_LENGTH - opened, 
-		0.0f - opened, SIDE_LENGTH + opened, 0.0f - opened,  
-		-SIDE_LENGTH - opened, 0.0f + opened, 0.0f - opened, 
+		0.0f - opened, 0.0f + opened, -SIDE_LENGTH - opened,
+		0.0f - opened, SIDE_LENGTH + opened, 0.0f - opened,
+		-SIDE_LENGTH - opened, 0.0f + opened, 0.0f - opened,
 
-		-SIDE_LENGTH - opened, 0.0f - opened, 0.0f + opened, 
-		0.0f - opened, -SIDE_LENGTH - opened, 0.0f + opened, 
-		0.0f - opened, 0.0f - opened, SIDE_LENGTH + opened,  
+		-SIDE_LENGTH - opened, 0.0f - opened, 0.0f + opened,
+		0.0f - opened, -SIDE_LENGTH - opened, 0.0f + opened,
+		0.0f - opened, 0.0f - opened, SIDE_LENGTH + opened,
 
-		0.0f + opened, 0.0f - opened, SIDE_LENGTH + opened,  
-		0.0f + opened, -SIDE_LENGTH - opened, 0.0f + opened, 
-		SIDE_LENGTH + opened, 0.0f - opened, 0.0f + opened,  
+		0.0f + opened, 0.0f - opened, SIDE_LENGTH + opened,
+		0.0f + opened, -SIDE_LENGTH - opened, 0.0f + opened,
+		SIDE_LENGTH + opened, 0.0f - opened, 0.0f + opened,
 
-		SIDE_LENGTH + opened, 0.0f - opened, 0.0f - opened, 
-		0.0f + opened, -SIDE_LENGTH - opened, 0.0f - opened, 
-		0.0f + opened, 0.0f - opened, -SIDE_LENGTH - opened, 
+		SIDE_LENGTH + opened, 0.0f - opened, 0.0f - opened,
+		0.0f + opened, -SIDE_LENGTH - opened, 0.0f - opened,
+		0.0f + opened, 0.0f - opened, -SIDE_LENGTH - opened,
 
-		0.0f - opened, 0.0f - opened, -SIDE_LENGTH - opened, 
+		0.0f - opened, 0.0f - opened, -SIDE_LENGTH - opened,
 		0.0f - opened, -SIDE_LENGTH - opened, 0.0f - opened,
 		-SIDE_LENGTH - opened, 0.0f - opened, 0.0f - opened,
 	};
@@ -444,12 +480,14 @@ void Draw() {
 
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
+	//сфера/шар/источник освещения
 	GLUquadricObj *quadObj;
 	quadObj = gluNewQuadric();
 	glColor3d(1, 1, 0);
 	gluQuadricDrawStyle(quadObj, GLU_FILL);
 	gluSphere(quadObj, 2, 20, 20);
 
+	//октаэдр
 	glLoadIdentity();
 	glTranslatef(0, 0, -20);
 	glRotatef((ox_rotation), 0, 1, 0);
@@ -506,16 +544,16 @@ void glutNormalKeys(unsigned char key, int x, int y)
 	switch (key)
 	{
 	case 'a':
-		dx_rotation = -0.5;
+		dx_rotation = -speed;
 		break;
 	case 'd':
-		dx_rotation = 0.5;
+		dx_rotation = speed;
 		break;
 	case 'w':
-		dy_rotation = -0.5;
+		dy_rotation = -speed;
 		break;
 	case 's':
-		dy_rotation = 0.5;
+		dy_rotation = speed;
 		break;
 	case 'q':
 		dy_rotation = 0;
@@ -559,13 +597,14 @@ void glutNormalKeys(unsigned char key, int x, int y)
 }
 
 int main(int argc, char** argv) {
+	glClearColor(0, 0, 0, 0);
 	std::cout << "W - Turn up\nA - Turn left\nS - turn down\nD - Turn right\nQ - Stop rotation of octahedron\nE - Open octahedron\nF - Turn the light on/off\nR - Change sun rotation\nX - Toggle transparency\nC - cut side of octahedron (demonstration of display lists)\nT - add textures\nZ - Close the programm\n";
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_MULTISAMPLE);
 	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
 	glutCreateWindow("Octahedron");
 
-	GLfloat openedPartLength = SIDE_LENGTH / 5; // side length / number of parts
+	GLfloat openedPartLength = SIDE_LENGTH / NUMBER_OF_PARTS; // side length / number of parts
 	cutOctahedron = glGenLists(2);
 	create_cut_triangles_list(cutOctahedron, openedPartLength, 0.0f);
 	glEndList();
@@ -583,10 +622,9 @@ int main(int argc, char** argv) {
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_COLOR_MATERIAL);
-
 	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+	load_textures();
 
-	glClearColor(0, 0, 0, 0);
 	glEnable(GL_DEPTH_TEST);
 
 	glutMainLoop();
